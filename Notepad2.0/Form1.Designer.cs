90e0f41ab93329dc63c930b129ac1cbeda9609da
MightyChubz <alexc@occultvisions.com>
2016-07-14 12:56:32 -0400

    Full project after stream.

---
+++ Notepad2.0/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Notepad2._0
+{
+    partial class Window
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.FileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.OpenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SaveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.TabControl = new System.Windows.Forms.TabControl();
+            this.OpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SaveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.CloseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ExitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(624, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // FileToolStripMenuItem
+            // 
+            this.FileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.OpenToolStripMenuItem,
+            this.SaveToolStripMenuItem,
+            this.CloseToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.ExitToolStripMenuItem});
+            this.FileToolStripMenuItem.Name = "FileToolStripMenuItem";
+            this.FileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.FileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // OpenToolStripMenuItem
+            // 
+            this.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem";
+            this.OpenToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.OpenToolStripMenuItem.Text = "Open...";
+            this.OpenToolStripMenuItem.Click += new System.EventHandler(this.OpenToolStripMenuItem_Click);
+            // 
+            // SaveToolStripMenuItem
+            // 
+            this.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem";
+            this.SaveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.SaveToolStripMenuItem.Text = "Save...";
+            this.SaveToolStripMenuItem.Click += new System.EventHandler(this.SaveToolStripMenuItem_Click);
+            // 
+            // TabControl
+            // 
+            this.TabControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TabControl.Location = new System.Drawing.Point(13, 28);
+            this.TabControl.Name = "TabControl";
+            this.TabControl.SelectedIndex = 0;
+            this.TabControl.Size = new System.Drawing.Size(599, 401);
+            this.TabControl.TabIndex = 2;
+            this.TabControl.SelectedIndexChanged += new System.EventHandler(this.TabControl_SelectedIndexChanged);
+            this.TabControl.ControlAdded += new System.Windows.Forms.ControlEventHandler(this.TabControl_ControlAdded);
+            // 
+            // OpenFileDialog
+            // 
+            this.OpenFileDialog.FileName = "openFileDialog1";
+            // 
+            // CloseToolStripMenuItem
+            // 
+            this.CloseToolStripMenuItem.Name = "CloseToolStripMenuItem";
+            this.CloseToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.CloseToolStripMenuItem.Text = "Close Tab";
+            this.CloseToolStripMenuItem.Click += new System.EventHandler(this.CloseToolStripMenuItem_Click);
+            // 
+            // ExitToolStripMenuItem
+            // 
+            this.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem";
+            this.ExitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.ExitToolStripMenuItem.Text = "Exit";
+            this.ExitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(149, 6);
+            // 
+            // Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.TabControl);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Window";
+            this.Text = "Notepad";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem FileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem OpenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem SaveToolStripMenuItem;
+        private System.Windows.Forms.TabControl TabControl;
+        private System.Windows.Forms.OpenFileDialog OpenFileDialog;
+        private System.Windows.Forms.SaveFileDialog SaveFileDialog;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem CloseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem ExitToolStripMenuItem;
+    }
+}
+
---
+++ Notepad2.0/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Notepad2._0
+{
+    public partial class Window : Form
+    {
+        public int PageCounter;
+        public IDictionary<string, string> TextDataDictionary = new Dictionary<string, string>();
+
+        public Window()
+        {
+            InitializeComponent();
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var tabsPage = new TabPage("new" + PageCounter)
+            {
+                Anchor = AnchorStyles.Right | AnchorStyles.Left | AnchorStyles.Top | AnchorStyles.Bottom
+            };
+
+            var richTextBox = new RichTextBox
+            {
+                Width = tabsPage.Width,
+                Height = tabsPage.Height,
+                Anchor = AnchorStyles.Right | AnchorStyles.Left | AnchorStyles.Top | AnchorStyles.Bottom
+            };
+
+            tabsPage.Controls.Add(richTextBox);
+
+            TabControl.TabPages.Insert(TabControl.TabCount, tabsPage);
+            PageCounter++;
+        }
+
+        private void SaveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (SaveFileDialog.ShowDialog() != DialogResult.OK)
+            {
+                SaveFileDialog.Dispose();
+                return;
+            }
+
+            var textBox = (RichTextBox) TabControl.SelectedTab.Controls[0];
+
+            TabControl.SelectedTab.Text = Path.GetFileNameWithoutExtension(SaveFileDialog.FileName);
+            textBox.SaveFile(SaveFileDialog.FileName, Path.GetExtension(SaveFileDialog.FileName) == ".rtf"
+                ? RichTextBoxStreamType.RichText
+                : RichTextBoxStreamType.PlainText);
+
+            if (!TextDataDictionary.ContainsKey(Path.GetFileNameWithoutExtension(SaveFileDialog.FileName)))
+                TextDataDictionary.Add(Path.GetFileNameWithoutExtension(SaveFileDialog.FileName), textBox.Text);
+            else
+                TextDataDictionary[Path.GetFileNameWithoutExtension(SaveFileDialog.FileName)] = textBox.Text;
+
+            if (TabControl.SelectedTab.Text != null && TabControl.SelectedTab.Text.EndsWith(@"*"))
+            {
+                var trimEnd = TabControl.SelectedTab.Text.TrimEnd('*');
+                TabControl.SelectedTab.Text = trimEnd;
+            }
+
+            SaveFileDialog.Dispose();
+        }
+
+        private void TabControl_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var textBox = (RichTextBox) TabControl.SelectedTab.Controls[0];
+            textBox.TextChanged += TextBoxOnTextChanged;
+        }
+
+        private void TextBoxOnTextChanged(object sender, EventArgs eventArgs)
+        {
+            var textBox = (RichTextBox)TabControl.SelectedTab.Controls[0];
+            if (!TextDataDictionary.ContainsKey(TabControl.SelectedTab.Text)) return;
+
+            if (TextDataDictionary[TabControl.SelectedTab.Text] == textBox.Text)
+            {
+                if (TabControl.SelectedTab.Text == null || !TabControl.SelectedTab.Text.EndsWith(@"*")) return;
+
+                var trimEnd = TabControl.SelectedTab.Text.TrimEnd('*');
+                TabControl.SelectedTab.Text = trimEnd;
+            }
+            else
+                TabControl.SelectedTab.Text = TabControl.SelectedTab.Text + @"*";
+        }
+
+        private void TabControl_ControlAdded(object sender, ControlEventArgs e)
+        {
+            var textBox = (RichTextBox)TabControl.SelectedTab.Controls[0];
+            textBox.TextChanged += TextBoxOnTextChanged;
+        }
+
+        private void OpenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (OpenFileDialog.ShowDialog() != DialogResult.OK)
+            {
+                OpenFileDialog.Dispose();
+                return;
+            }
+
+            TabControl.SelectedTab.Text = Path.GetFileNameWithoutExtension(OpenFileDialog.FileName);
+
+            var stream = new StreamReader(new FileStream(OpenFileDialog.FileName, FileMode.Open));
+            var textBox = (RichTextBox)TabControl.SelectedTab.Controls[0];
+            textBox.Text = stream.ReadToEnd();
+
+            if (!TextDataDictionary.ContainsKey(Path.GetFileNameWithoutExtension(OpenFileDialog.FileName)))
+                TextDataDictionary.Add(Path.GetFileNameWithoutExtension(OpenFileDialog.FileName), textBox.Text);
+            else
+                TextDataDictionary[Path.GetFileNameWithoutExtension(OpenFileDialog.FileName)] = textBox.Text;
+
+            OpenFileDialog.Dispose();
+        }
+
+        private void CloseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TabControl.TabPages.Remove(TabControl.SelectedTab);
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
